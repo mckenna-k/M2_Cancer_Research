06c43df4c79e0d5904d208ed55d414e206faf8f7
theosilvestre <75995176+theosilvestre@users.noreply.github.com>
2022-02-08 23:16:34 +0100

    erreur de typo

--- script_simu.R
+++ script_simu.R
@@ -1,7 +1,7 @@
 process <- function(n,l01,l02,l12,censor=NULL){
   if(is.null(censor)){
     t1 <- rexp(n,l01+l02) #temps de passage de 0 à 1 ou 0 à 2
-    s1 <- rbinom(n,1,l01/(l01+l02))+1 #détermination de l'état atteint (1 ou 2)
+    s1 <- rbinom(n,1,l02/(l01+l02))+1 #détermination de l'état atteint (1 ou 2)
     t2 <- rexp(n,l12) #temps absolu de passage de l'état 1 à 2
     df <- data.frame("DSS"=rep(1,n),"DSS.time"=ifelse(s1==1,t1+t2,t1),
                      "PFI"=rep(1,n),"PFI.time"=t1,
@@ -13,7 +13,7 @@ process <- function(n,l01,l02,l12,censor=NULL){
     t1 <- rexp(n,l01+l02+censor) #temps de passage de 0 à 1 ou 0 à 2
     s1 <- apply(rmultinom(n, size = 1, prob = c(censor/(l01+l02+censor),l01/(l01+l02+censor),l02/(l01+l02+censor)))==1,2,which)-1 #détermination de l'état atteint (1 ou 2)
     t2 <- rexp(n,l12+censor)
-    s2 <- rbinom(n,1,censor/(l01+censor))
+    s2 <- rbinom(n,1,l01/(l01+censor))
     df <- data.frame("DSS"=ifelse(s1 > 0,1,0),"DSS.time"=ifelse(s1==1,t1+t2,t1),
                      "PFI"=ifelse(s1 > 0,1,0),"PFI.time"=t1,
                      "T01"=ifelse(s1==1,1,0),"T01.time"=t1,
